7a02fb6ecec66fa2767836a3a962d66523d62693
polarisid <danielcaevalhorst@gmail.com>
2021-11-07 23:14:59 -0200

    Quase ok

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let cards = window.prompt("Com quantas cartas você deseja jogar?", "de 4 a 14");
-const imgs =["bobrossparrot","explodyparrot","fiestaparrot","metalparrot","revertilparrot","tripletsparrot","inicornparrot"] ;
+
+let contador_finish =0;
+let contador_de_jogadas=0;
+let cards = window.prompt("Com quantas cartas você deseja jogar?", "4 a 14");
+const imgs =["bobrossparrot","explodyparrot","fiestaparrot","metalparrot","revertitparrot","tripletsparrot","unicornparrot"] ;
 
 if(((cards%2)!==0) || cards>14 || cards <4){
     alert("Por favor Insira números pares e na faixa de 4-14");
@@ -7,24 +10,88 @@ if(((cards%2)!==0) || cards>14 || cards <4){
 }
 
 
+let list1=gerar_lista(parseInt(cards)/2);
+list1.sort(comparador);
+
+let list2=gerar_lista(parseInt(cards)/2);
+list2.sort(comparador);
+
+
+
 const card_container = document.querySelector(".cartas-container");
 
-for(let i=0;i<parseInt(cards);i++){
+for(let i=0;i<parseInt(cards)/2;i++){
   
 
   card_container.innerHTML+= `
-  <div class="card" data-identifier="card"> \n
+  <div class="card" onclick="clickCard(this)" data-identifier="card"> \n
   <div class="card-face front-face" data-identifier="front-face">\n
     <img src="/assets/front.png" alt="">\n
   </div>\n
   <div class ="card-face back-face" data-identifier="back-face">
-      <img src="/assets/${imgs[2]}.gif" alt="">\n
+      <img src="/assets/${imgs[list1[i]]}.gif" alt="">\n
   </div>\n
-</div>`
+</div>
+
+  <div class="card" onclick="clickCard(this)" data-identifier="card"> \n
+  <div class="card-face front-face" data-identifier="front-face">\n
+    <img src="/assets/front.png" alt="">\n
+  </div>\n
+  <div class ="card-face back-face" data-identifier="back-face">
+      <img src="/assets/${imgs[list2[i]]}.gif" alt="">\n
+  </div>\n
+</div>
+`
  
 };
 
-gerar_lista(parseInt(cards))
+function clickCard(element){
+  contador_de_jogadas++;
+  
+  const elemento_selecionado = document.querySelector(".card.mostrar-card");
+  
+  const img1= document.querySelector(".card.mostrar-card .back-face img");
+  const img2= element.querySelector(".back-face img");
+  
+  //VERIFICAR SE JA TEM ALGUM ABERTO
+  if(elemento_selecionado!=null){
+    //Se for par
+    if((img1.src.substr(img1.src.lastIndexOf('/')))==(img2.src.substr(img2.src.lastIndexOf('/')))){
+      elemento_selecionado.classList.add("feito-par");
+      element.classList.add("feito-par");
+      
+      elemento_selecionado.onclick="";
+      element.onclick="";
+
+      element.classList.remove("mostrar-card")
+      elemento_selecionado.classList.remove("mostrar-card");
+      contador_finish++;
+      
+      //se o jogo já encerrou
+      if(contador_finish==(parseInt(cards)/2)){
+        setTimeout(alertare, 1800);
+      }
+    }
+    //se não for par
+    else{
+      
+      //elemento_selecionado.classList.toggle("mostrar-card")
+      //element.classList.remove("mostrar-card")
+      element.classList.add("mostrar-card")
+      setTimeout(error_card,1000);
+      
+      function error_card(){
+        //elemento_selecionado.classList.add("revirar")
+        //element.classList.add("revirar")
+        elemento_selecionado.classList.toggle("mostrar-card")
+        element.classList.remove("mostrar-card")
+      }
+    }
+  }
+  else{
+    element.classList.add("mostrar-card");
+  }
+}
 
 
 function gerar_lista(max){
@@ -32,10 +99,21 @@ function gerar_lista(max){
   for (let i = 0; i < max; i++) {
   list[i] = i ;
 }
-console.log(list)
+return list;
+}
+function alertare(){
+
+  alert("Você ganhou! Você fez em "+ contador_de_jogadas+ " Jogadas");
+  location.reload();
+
 }
 
 
+
+// Esta função pode ficar separada do código acima, onde você preferir
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
 //const cardes=card.querySelector(".card-1");
   
